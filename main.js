20a0693d2df6d8657342db5b4dad59f62ee286f0
Nerijus Plytninkas <nerijus.plytninkas@gmail.com>
2016-05-13 06:54:42 +0300

    Lesson_6
    Neveikia checkbox naudojant class, su ID pazymi tik pirma.

--- main.js
+++ main.js
@@ -1,48 +1,44 @@
 $(document).ready(function () {
 
-    // create new field for data with edit types
+    $("#insert_input").on("keypress", function (e) {
+        var $this = $(this);
+        var newInputText = $this.val();
 
-    $("#insert_input").keypress(function (e) {
-        if (e.which == 13) {
+        if (e.which === 13) {
             e.preventDefault();
 
-            var new_input_text = $("#insert_input").val(),
-                new_input = '<input class="form-control" type="text" id="new_data" value="' + new_input_text + '">',
-                checkbox = '<div><div class="input-group"> <span class="input-group-addon"> <input type="checkbox" class="check"> </span>',
-                remove = '<span class="input-group-addon" id="basic-addon2"> <a href="#">X</a> </span> </div></div>',
-                edit_input = checkbox + new_input + remove;
+            $this.val("");
 
-            // alert if insert_input is emty
-
-            if (new_input_text === '') {
-                alert("Tusčias įvedimo laukas");
+            if (newInputText === '') {
+                alert("Tuscias ivedimo laukas");
             } else {
-
-                // insert new data in new field from insert_input with edit types
-
-                $(".input_field").append(edit_input);
-            };
-
-            // clear insert_input after inserting new data
-
-            $("#insert_input").val('');
-
-            // remove new_input
-
-            $(".input_field").on("click", "#basic-addon2", function () {
-                $(this).parent().remove();
-            });
-
-            // check new_input data
-
-            $(".input_field").on("click", ".check", function () {
-                if ($(this).is(":checked")) {
-                    $("#new_data").addClass("selected");
-                } else {
-                    $("#new_data").removeClass("selected");
-
-                };
-            });
-        };
+                $(".input_field").append(getNewLineItem(newInputText));
+            }
+        }
     });
+
+    $(document).on("click", ".input-remove", function () {
+        $(this).parent().remove();
+    });
+
+    $(document).on("click", ".input-checkbox", function () {
+        var $this = $(this);
+        if ($this.is(":checked")) {
+            $this.addClass("selected");
+        } else {
+            $this.removeClass("selected");
+        }
+    });
+
+    function getNewLineItem(inputText) {
+        return '<div class="input-group">' +
+            '<span class="input-group-addon">' +
+            '<input class="input-checkbox" type="checkbox" >' +
+            '</span>' +
+            '<input class="form-control" type="text" value="' + inputText + '">' +
+            '<span class="input-remove input-group-addon">' +
+            '<a href="#">X</a>' +
+            '</span>' +
+            '</div>';
+    }
 });
